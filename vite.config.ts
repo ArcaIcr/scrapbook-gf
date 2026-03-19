635abf8d92d7410386f8eb945a0e7368b0909c41
ArcaIcr <morrix.ken@gmail.com>
2026-03-20 05:44:59 +0800

    chore: Rechunk some Loads

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-motion': ['framer-motion'],
+          'vendor-firebase': ['firebase/app', 'firebase/firestore'],
+          'vendor-icons': ['lucide-react']
+        }
+      }
+    }
+  }
 })
